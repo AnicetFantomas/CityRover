0d4e8b0462217871b62f2ba8d5a7bbe1bf6ce269
AnicetFantomas <anicetmurhula20@gmail.com>
2023-11-27 16:22:29 +0200

    setup project

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          '@components': './src/components',
+          '@screens': './src/screens',
+        },
+      },
+    ],
+    ['module:react-native-dotenv'],
+    'react-native-reanimated/plugin',
+  ],
+};
