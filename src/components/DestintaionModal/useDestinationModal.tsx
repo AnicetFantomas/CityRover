42283a60b22e5029b3aad485535bbf2558f633cd
AnicetFantomas <anicetmurhula20@gmail.com>
2023-11-28 23:05:39 +0200

    get value on change

--- src/components/DestintaionModal/useDestinationModal.tsx
+++ src/components/DestintaionModal/useDestinationModal.tsx
@@ -4,5 +4,10 @@ export const useDestinationModal = () => {
     const [destinationInputText, setDestinationInputText] = useState('');
 
     const handleDestinationInputTextChange = (value: string) => {
+        setDestinationInputText(value);
+    };
+  return {
+    models: {destinationInputText},
+    operations: {handleDestinationInputTextChange},
   }
 }
